903c42268ac8085a17bc60d2a513d4f3c76a33a1
Rocio Hsu <shachien@gmail.com>
2022-11-23 21:41:07 +0800

    221123 update

--- js/layout_bak.js
+++ js/layout_bak.js
@@ -1,4 +1,32 @@
 $(function() {
+    //form input placeholder
+    $('.form-column input').keyup(function(){
+        var limit_num = $(this).val().length;
+        var textNote = $(this).attr('placeholder');
+        var form_title = $(this).prev('.form-title').text();
+
+        if(form_title == '' || form_title == textNote){
+            if(limit_num > 0) {
+                $(this).closest('.form-column').find('.form-title').text(textNote).fadeIn();
+            } else {
+                $(this).closest('.form-column').find('.form-title').fadeOut(0);
+            }
+        }
+    });
+
+    /* input show delete icon */
+    /* $('.form-column').hover(function(){
+        $(this).append('<a class="delete"></a>');
+    },function(){
+        $(this).find('.delete').remove();
+    }); */
+
+    /* reset input value */
+    /* $(document).on('click', '.form-column .delete', function () {
+        $(this).parent('.form-column').find('input').val('');
+        //$(this).parent('.form-column').find('.form-title').hide();
+    }); */
+
     /* open popup */
     $('a.popup').click(function(){
         var id = $(this).attr('rel');
